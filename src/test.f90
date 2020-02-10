e6dbacb8ee1bce3cdfdbd64c733cf70bca99936d
evanmacbride <ejmacbride@gmail.com>
2020-02-09 21:28:45 -0500

    procedures

--- src/test.f90
+++ src/test.f90
@@ -1,37 +1,24 @@
-program pointerExample
+program mainprog
 implicit none
 
-	integer, pointer :: p1
-	integer, target :: t1
-	integer, target :: t2
+	real :: a, b
+	a = 2.0
+	b = 3.0
 
-	p1=>t1
-	p1 = 1
+	print *, "Before calling swap"
+	print *, "a = ", a
+	print *, "b = ", b
+	call swap(a, b)
 
-	print *, p1
-	print *, t1
+	print *, "After calling swap"
+	print *, "a = ", a
+	print *, "b = ", b
 
-	p1 = p1 + 4
-	print *, p1
-	print *, t1
-
-	t1 = 8
-	print *, p1
-	print *, t1
-
-	nullify(p1)
-	print *, t1
-
-	p1=>t2
-	print *, associated(p1)
-	print *, associated(p1, t1)
-	print *, associated(p1, t2)
-
-	print *, p1
-	print *, t2
-
-	p1 = 10
-	print *, p1
-	print *, t2
-
-end program pointerExample
+contains
+	subroutine swap(x, y)
+		real :: x, y, temp
+		temp = x
+		x = y
+		y = temp
+	end subroutine swap
+end program mainprog
