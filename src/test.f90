29267fbb723f916ac0d622550ddbab89bc4cddf4
evanmacbride <ejmacbride@gmail.com>
2020-02-10 21:55:52 -0500

    numerical precision

--- src/test.f90
+++ src/test.f90
@@ -1,24 +1,22 @@
-program mainprog
-implicit none
+PROGRAM getSize
+IMPLICIT NONE
+	REAL(KIND = 4) :: a
+	REAL(KIND = 8) :: b
+	INTEGER(KIND = 2) :: i
+	INTEGER(KIND = 4) :: j
 
-	real :: a, b
-	a = 2.0
-	b = 3.0
+	PRINT *, 'Precision of real(4) = ', PRECISION(a)
+	PRINT *, 'Precision of real(8) = ', PRECISION(b)
 
-	print *, "Before calling swap"
-	print *, "a = ", a
-	print *, "b = ", b
-	call swap(a, b)
+	PRINT *, 'Range of real(4) = ', RANGE(a)
+	PRINT *, 'Range of real(8) = ', RANGE(b)
 
-	print *, "After calling swap"
-	print *, "a = ", a
-	print *, "b = ", b
+	PRINT *, 'Maximum exponent of real(4) = ', MAXEXPONENT(a)
+	PRINT *, 'Maximum exponent of real(8) = ', MAXEXPONENT(b)
 
-contains
-	subroutine swap(x, y)
-		real :: x, y, temp
-		temp = x
-		x = y
-		y = temp
-	end subroutine swap
-end program mainprog
+	PRINT *, 'Minimum exponent of real(4) = ', MinEXPONENT(a)
+	PRINT *, 'Minimum exponent of real(8) = ', MinEXPONENT(b)
+
+	PRINT *, 'Bits in integer(2) = ', BIT_SIZE(i)
+	PRINT *, 'Bits in integer(4) = ', BIT_SIZE(j)
+END PROGRAM getSize
