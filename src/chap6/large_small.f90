c5b494e98dcafa0f8154a2aba2693d867433f20b
evanmacbride <ejmacbride@gmail.com>
2020-03-06 08:58:12 -0500

    finish largest & smallest ex chap 6

--- src/chap6/large_small.f90
+++ src/chap6/large_small.f90
@@ -1,25 +1,35 @@
 PROGRAM large_small
 IMPLICIT NONE
-  REAL, DIMENSION(100) :: values
+  INTEGER, PARAMETER :: ARR_SIZE = 100
+  REAL, DIMENSION(ARR_SIZE) :: input_vals
   REAL :: largest = 0, smallest = 0
-  INTEGER :: index, finish
+  INTEGER :: index, num_vals
 
   WRITE(*, *) "How many numbers will be entered?"
-  READ(*, *) finish
+  READ(*, *) num_vals
 
-  DO index = 1, finish
+  DO index = 1, num_vals
     WRITE(*, *) "Enter a number:"
-    READ(*, *) values(index)
+    READ(*, *) input_vals(index)
   END DO
 
-  DO index = 1, finish
-    IF (values(index) > largest) THEN
-      largest = values(index)
-    ELSE IF (values(index) < smallest) THEN
-      smallest = values(index)
+  DO index = 1, num_vals
+    IF (input_vals(index) > largest) THEN
+      largest = input_vals(index)
+    ELSE IF (input_vals(index) < smallest) THEN
+      smallest = input_vals(index)
     END IF
   END DO
 
-  WRITE(*, 100) largest, smallest
-  100 FORMAT ("Largest: ", F11.4, 2X, "Smallest: ", F11.4)
+  DO index = 1, num_vals
+    IF (input_vals(index) == largest) THEN
+      WRITE(*, 100) input_vals(index), "LARGEST"
+    ELSE IF (input_vals(index) == smallest) THEN
+      WRITE(*, 100) input_vals(index), "SMALLEST"
+    ELSE
+      WRITE(*, 100) input_vals(index)
+    END IF
+    100 FORMAT (2X, F11.4, 2X, A8)
+  END DO
+
 END PROGRAM large_small
